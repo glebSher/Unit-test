2a8cba3bb9437cabd59f716b09ce4515ad41ca85
glebshcherbakov <gleb.sher309@gmail.com>
2023-10-17 15:36:15 +0300

    hw_1

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,2 +1,48 @@
-package main;public class Calculator {
+package main;
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+
+    public static double squareRootExtraction(double num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+        }
+        return Math.sqrt(num);
+    }
+
+
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        // purchaseAmount - сумма покупки
+        // discountAmount - размер скидки
+        double resultAmount = purchaseAmount - ((double) discountAmount / 100 * purchaseAmount);
+        if (purchaseAmount < 1)
+            throw new IllegalArgumentException("Сумма покупки не может быть меньше 1RUR");
+        if (discountAmount < 0) throw new IllegalArgumentException("Процент скидки не может быть отрицательным");
+        return resultAmount;
+    }
 }
--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -1,2 +1,119 @@
-package PACKAGE_NAME;public class CalculatorTest {
+import main.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class CalculatorTest {
+    @Test
+    public void additionShouldCorrect() {
+        int result = 6;
+        int resultTest = Calculator.calculation(3, 3, '+');
+        assertEquals(result, resultTest, "Некорректное сложение");
+    }
+
+
+    // Утверждение на вычитание
+
+    @Test
+    public void differenceShouldCorrect() {
+        int result = 3;
+        int resultTest = Calculator.calculation(6, 3, '-');
+        assertEquals(result, resultTest, "Некорректное вычитание");
+    }
+
+
+    // Утверждение на умножение
+
+    @Test
+    public void multiplicationShouldCorrect() {
+        int result = 8;
+        int resultTest = Calculator.calculation(4, 2, '*');
+        assertEquals(result, resultTest, "Некорректное умножение");
+    }
+
+
+    // Утверждение на деление
+
+    @Test
+    public void divisionShouldCorrect() {
+        int result = 6;
+        int resultTest = Calculator.calculation(18, 3, '/');
+        assertEquals(result, resultTest, "Некорректное деление");
+    }
+
+
+    // Извлечение корня целого числа
+
+    @Test
+    public void rootIntegerShouldExtracted() {
+        double result = 5.0;
+        double resultTest = Calculator.squareRootExtraction(25.0);
+        assertEquals(result, resultTest, "Корень целого числа извлечен некорректно");
+    }
+
+
+    // Утверждение расчета суммы покупки с применением скидки
+
+    @Test
+    public void calculatingDiscountShouldCorrect() {
+        double result = 4500.0;
+        double resultTest = Calculator.calculatingDiscount(5000, 10);
+        assertEquals(result, resultTest, "Некорректное вычисление скидки");
+    }
+
+
+    // Исключение деления на ноль
+
+    @Test
+    public void checkDivisionByZero() {
+        String message = "Division by zero is not possible";
+        Exception exception = assertThrows(ArithmeticException.class, () ->
+                Calculator.calculation(5, 0, '/'));
+        assertEquals(message, exception.getMessage());
+    }
+
+
+    // Исключение некорректного оператора
+
+    @Test
+    public void checkInvalidOperator() {
+        String message = "Unexpected value operator: _";
+        Exception exception = assertThrows(IllegalStateException.class, () ->
+                Calculator.calculation(9, 3, '_'));
+        assertEquals(message, exception.getMessage());
+    }
+
+
+    // Исключение: извлечение корня из отрицательного числа
+
+    @Test
+    public void checkTakingRootNegativeNumber() {
+        String message = "Cannot calculate square root of a negative number";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Calculator.squareRootExtraction(-0.1));
+        assertEquals(message, exception.getMessage());
+    }
+
+    // Исключение: сумма покупки меньше 1RUR
+
+    @Test
+    public void purchaseAmountCannotLessOne() {
+        String messageException = "Сумма покупки не может быть меньше 1RUR";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Calculator.calculatingDiscount(0, 10));
+        assertEquals(messageException, exception.getMessage());
+    }
+
+
+    // Исключение: размер скидки меньше нуля
+
+    @Test
+    public void discountAmountCannotLessZero() {
+        String message = "Процент скидки не может быть отрицательным";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Calculator.calculatingDiscount(100, -5));
+        assertEquals(message, exception.getMessage());
+    }
 }
